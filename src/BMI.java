de563ac7832dbad3e2d9974f240b218e7a7a3c4e
Pham Tuan Hung 3011 <phamtu571990@gmail.com>
2021-11-04 22:04:35 +0700

    Prime

--- src/BMI.java
+++ src/BMI.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class BMI {
@@ -8,6 +9,9 @@ public class BMI {
         double BMI;
 
         Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.ENGLISH);
+        scanner.locale();
+        System.out.println(""+ scanner.locale());
 
 
         System.out.println("Nhap so tuoi cua ban");
@@ -24,12 +28,12 @@ public class BMI {
             System.out.println("Nhap chieu cao: ");
             height = scanner.nextDouble(); // nhap chieu cao he so cm22;
 
-            BMI = weight / Math.pow(height,2) *10000;
+            BMI = weight / Math.pow(height,2) ;
             if (BMI < 18.5) {
                 System.out.println("Nghien!");
-            } else if (BMI >= 18.5 && BMI <=25.0) {
+            } else if ( BMI <=25.0) {
                 System.out.println("Normal");
-            } else if (BMI >= 25.0 && BMI <= 30.0) {
+            } else if ( BMI <= 30.0) {
                 System.out.printf("%-20.2f%s", BMI, "Overweight");
             } else {
                 System.out.println("Obese");
---
+++ src/converterNumbetToString.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class converterNumbetToString {
+
+    public static void main(String[] args) {
+        int numBer;
+        double toConverter;
+        String toString;
+
+        System.out.println("Hay nhap so duoi 999");
+        Scanner scanner = new Scanner(System.in);
+        numBer = scanner.nextInt();
+
+        if (numBer <= 10) {
+            switch (numBer){
+                case 0:
+                    System.out.println("So 0");
+                    break;
+                case 1:
+                    System.out.println("So 1");
+                    break;
+                case 2:
+                    System.out.println("So 2");
+                    break;
+                case 3:
+                    System.out.println("So 3");
+                    break;
+                case 4:
+                    System.out.println("So 4");
+                    break;
+                case 5:
+                    System.out.println("So 5");
+                    break;
+                case 6:
+                    System.out.println("So 6");
+                    break;
+                case 7:
+                    System.out.println("So 7");
+                    break;
+                case 8:
+                    System.out.println("So 8");
+                    break;
+                case 9:
+                    System.out.println("So 9");
+                    break;
+                case 10:
+                    System.out.println("So 10");
+                    break;
+            }
+        }
+        if (numBer < 20) {
+            toConverter = numBer % 10;
+        }
+
+    }
+}
