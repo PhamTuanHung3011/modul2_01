36bc9ccef7071c2ee0fa6713f377839e15ec65f6
Pham Tuan Hung 3011 <phamtu571990@gmail.com>
2021-11-05 15:46:48 +0700

    check so ngto < 100

--- src/checkPrime.java
+++ src/checkPrime.java
@@ -4,26 +4,26 @@ public class checkPrime {
     public static void main(String[] args) {
 
         // nhap du lieu
-        int numberOne;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Nhap so ban muon kiem tra");
-        numberOne = input.nextInt();
+//        int numberOne;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhap so ban muon kiem tra");
+//        numberOne = input.nextInt();
         int count = 0;
 
-        for (int i = 0; i <numberOne; i++){
+        for (int i = 0; i < 100; i++){
             if (flag(i)) {
                 count++;
+                System.out.println(i);
             }
         }
         System.out.println(count+ " ");
-
 
     }
     static boolean flag(int number) {
         if (number < 2){
             return false;
         } else {
-            for (int i = 2; i < Math.sqrt(number); i++) {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
                 if (number % i == 0) {
                     return false;
                 }
